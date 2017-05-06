8c7d9e8fa88afeb2c2ecf9687db0b6ad0167bbbd
Tiago Rodrigues <tirodrigues.jpa@gmail.com>
2017-05-06 17:16:03 -0300

    ajust name package

---
+++ src/br/com/algoritmos/TestaMenorPreco.java
@@ -0,0 +1,34 @@
+package br.com.algoritmos;
+
+public class TestaMenorPreco {
+
+	public static void main(String[] args) {
+
+		Produto produtos[] = {
+				new Produto("Lamborghini", 1000000),
+				new Produto("Jipe", 46000),
+				new Produto("Brasília", 16000),
+				new Produto("Smart", 46000),
+				new Produto("Fusca", 17000),
+				null, 
+				null,
+				null,
+		};
+
+		int maisBarato = buscaMenor(produtos, 0, 4);
+
+		System.out.println("mais barato e " + produtos[maisBarato].getPreco());
+	}
+
+	private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
+		
+		int maisBarato = 0;
+		
+		for (int i = inicio; i <= termino; i++) {
+			if (produtos[i].getPreco() < produtos[maisBarato].getPreco()) {
+				maisBarato = i;
+			}
+		}
+		return maisBarato;
+	}
+}
